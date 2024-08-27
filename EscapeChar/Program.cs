0ce98f331c2ec83616d7fb9165eceb8832e758a9
we1dy <ladyarboleda26@gmail.com>
2024-08-28 01:15:04 +0800

    Add project files.

---
+++ EscapeChar/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace EscapeChar
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello\tWorld\n\n");
+			Console.WriteLine("Hello World ?");
+			Console.WriteLine("\'Hello World \'");
+			Console.WriteLine("Hello World \v");
+			Console.WriteLine("\"Hello World \"");
+
+			Console.ReadLine();
+		}
+	}
+}
